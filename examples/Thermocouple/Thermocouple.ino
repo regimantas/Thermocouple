08bc94e18bf4acb66ad925abafda209d5c3f5c76
regimantas <regtech0@gmail.com>
2016-12-27 01:02:53 +0200

    Fixed Library to compile

--- examples/Thermocouple/Thermocouple.ino
+++ examples/Thermocouple/Thermocouple.ino
@@ -1,9 +1,11 @@
 #include <Thermocouple.h>
 
+Thermocouple Therm;
+
 void setup() {
   // put your setup code here, to run once:
-  double temp = Thermocouple.KtypeTemp(2.022);
-  double Volt = Thermocouple.KtypeVoltage(50);
+  double temp = Therm.KtypeTemp(2.022);
+  double Volt = Therm.KtypeVoltage(50);
   Serial.begin(9600);
   delay(1000);
   Serial.println(temp);
--- src/Thermocouple.h
+++ src/Thermocouple.h
@@ -5,12 +5,15 @@
 
 class Thermocouple {
 public:
+        //Thermocouple();
         double KtypeTemp(double voltage);
         double KtypeVoltage(double input_temp);
         double StypeVoltage(double temp_s);
 		double StypeTemp(double volt_s);
 		double JtypeVoltage(double temp_j);
 		double JtypeVolt(double volt_t_j);
+private:
+		
 };
  
 #endif
